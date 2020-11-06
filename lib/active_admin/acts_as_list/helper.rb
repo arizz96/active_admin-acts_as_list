33f2e1da0d3fea16b180e88dfc211ecd611dc511
arizz96 <arizz96@gmail.com>
2020-11-06 11:51:53 +0100

    Style

--- lib/active_admin/acts_as_list/helper.rb
+++ lib/active_admin/acts_as_list/helper.rb
@@ -48,41 +48,37 @@ module ActiveAdmin
         member_action :move_to_top do
           if resource.first?
             redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_to_top', :resource => resource_class.to_s.camelize.constantize.model_name.human )
-            return
+          else
+            resource.move_to_top
+            redirect_to :back, :notice => I18n.t('acts_as_list.moved_to_top', :resource => resource_class.to_s.camelize.constantize.model_name.human )
           end
-
-          resource.move_to_top
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_to_top', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end
 
         member_action :move_to_bottom do
           if resource.last?
             redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_to_bottom', :resource => resource_class.to_s.camelize.constantize.model_name.human )
-            return
+          else
+            resource.move_to_bottom
+            redirect_to :back, :notice => I18n.t('acts_as_list.moved_to_bottom', :resource => resource_class.to_s.camelize.constantize.model_name.human )
           end
-
-          resource.move_to_bottom
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_to_bottom', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end
 
         member_action :move_up do
           if resource.first?
             redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_up', :resource => resource_class.to_s.camelize.constantize.model_name.human )
-            return
+          else
+            resource.move_higher
+            redirect_to :back, :notice => I18n.t('acts_as_list.moved_up', :resource => resource_class.to_s.camelize.constantize.model_name.human )
           end
-
-          resource.move_higher
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_up', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end
 
         member_action :move_down do
           if resource.last?
             redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_down', :resource => resource_class.to_s.camelize.constantize.model_name.human )
-            return
+          else
+            resource.move_lower
+            redirect_to :back, :notice => I18n.t('acts_as_list.moved_down', :resource => resource_class.to_s.camelize.constantize.model_name.human )
           end
-
-          resource.move_lower
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_down', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end
       end
     end
